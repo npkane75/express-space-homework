e4802f26e66b3e876db47e300a5e463e6fe730de
msolorio-ga <michael.solorio@generalassemb.ly>
2021-01-07 14:41:42 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ const app = express();
 // * Your mission is to complete the app
 // * The app will need routes for index and show
 // * The app will need views for index and show
-//
-// * MAIN GOAL:
-// * User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page to view the data
-//
+
+// INDEX Route
+// send data to 'missions/index.ejs' view
+// the view should display just the names of each mission
+// display the mission names as <li> in a <ul> with the class name "missions"
+
+// SHOW Route
+// send data to 'missions/show.ejs' view
+// the view should display all the data for a single mission
+
+// * Bonus/Hungry for More: User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page to view the data
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
 // * Bonus/Hungry for More: add static css to style the pages (google how)
 
@@ -62,16 +69,6 @@ const marsMissions = [
     img: ""
   }
 ];
-
-// INDEX Route
-// send data to 'missions/index.ejs' view
-// the view should display just the names of each mission
-// display the mission names as <li> in a <ul> with the class name "missions"
-
-// SHOW Route
-// send data to 'missions/show.ejs' view
-// the view should display all the data for a single mission
-
 
 
 // LISTENER
